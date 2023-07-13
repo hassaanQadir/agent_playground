c894e8c7ad6b646da0f5b35d2f7419f9d660ec82
Hassaan Qadir <hassaanqadir1@gmail.com>
2023-07-13 20:22:39 +0000

    refactored with gpt-4

--- indexCreator.py
+++ indexCreator.py
@@ -1,15 +1,15 @@
-#pip install -qU \
-#  tiktoken==0.4.0 \
-#  openai==0.27.7 \
-#  langchain==0.0.137 \
-#  "pinecone-client[grpc]"==2.2.1
-
-#pip install langchain
-
-#wget -r -A.html -P rtdocs https://docs.opentrons.com/v2/
-
-### this is where we vectorize the OpenTrons API
-
+# Import all required modules
+import os
+import time
+from typing_extensions import Concatenate
+from uuid import uuid4
+from dotenv import load_dotenv
+from tqdm.auto import tqdm
+from langchain.document_loaders import ReadTheDocsLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import tiktoken
+import openai
+import pinecone
 
 # Load environment variables
 load_dotenv('.env')
@@ -17,33 +17,22 @@ load_dotenv('.env')
 openai_api_key = os.getenv('OPENAI_API_KEY')
 pinecone_api_key = os.getenv('PINECONE_API_KEY')
 
+# SECTION 1: LOAD AND TOKENIZE THE DOCUMENT AND SPLIT IT INTO CHUNKS
 
-## section 1: load and tokenize the document and split it into chunks
-
-from langchain.document_loaders import ReadTheDocsLoader
-
-
+# Instantiate ReadTheDocsLoader to load the documents from 'rtdocs'
 loader = ReadTheDocsLoader('rtdocs')
 docs = loader.load()
 
-import tiktoken
-
+# Get tokenizer for the 'gpt-3.5-turbo' model from tiktoken
 tokenizer_name = tiktoken.encoding_for_model('gpt-3.5-turbo')
-tokenizer_name.name
-
 tokenizer = tiktoken.get_encoding(tokenizer_name.name)
 
-# create the length function
+# Define function to get the length of a tokenized text
 def tiktoken_len(text):
-    tokens = tokenizer.encode(
-        text,
-        disallowed_special=()
-    )
+    tokens = tokenizer.encode(text, disallowed_special=())
     return len(tokens)
 
-
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-
+# Instantiate RecursiveCharacterTextSplitter to split text into chunks
 text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=500,
     chunk_overlap=20,
@@ -51,12 +40,8 @@ text_splitter = RecursiveCharacterTextSplitter(
     separators=["\n\n", "\n", " ", ""]
 )
 
-from typing_extensions import Concatenate
-from uuid import uuid4
-from tqdm.auto import tqdm
-
+# Split document content into chunks and store each chunk's metadata
 chunks = []
-
 for idx, page in enumerate(tqdm(docs)):
     content = page.page_content
     if len(content) > 100:
@@ -69,15 +54,9 @@ for idx, page in enumerate(tqdm(docs)):
             'url': url
         } for i in range(len(texts))])
 
-
-## section 2: initialize an embedding model for use later
-
-
-import os
-import openai
+# SECTION 2: INITIALIZE AN EMBEDDING MODEL FOR USE LATER
 
 embed_model = "text-embedding-ada-002"
-
 res = openai.Embedding.create(
     input=[
         "Sample document text goes here",
@@ -85,72 +64,58 @@ res = openai.Embedding.create(
     ], engine=embed_model
 )
 
-# section 3: create an index 
+# SECTION 3: CREATE AN INDEX 
 
-import pinecone
-
-# initialize connection to pinecone (get API key at app.pinecone.io)
-api_key = pinecone_api_key
-# find your environment next to the api key in pinecone console
-env = "us-west1-gcp"
-
-pinecone.init(api_key=api_key, enviroment=env)
-
+# Initialize Pinecone
+pinecone.init(api_key=pinecone_api_key, enviroment="us-west1-gcp")
 
 index_name = 'opentronsapi-docs'
 
-import time
-
-# check if index already exists (it shouldn't if this is first time)
+# Create Pinecone index if it doesn't exist
 if index_name not in pinecone.list_indexes():
-    # if does not exist, create index
     pinecone.create_index(
         index_name,
         dimension=len(res['data'][0]['embedding']),
         metric='cosine'
     )
-    # wait for index to be initialized
+    # Wait for index to be initialized
     time.sleep(1)
 
-# connect to index
+# Connect to Pinecone index and view its stats
 index = pinecone.GRPCIndex(index_name)
-# view index stats
 index.describe_index_stats()
 
-# section 4: embed the chunks into the index
+# SECTION 4: EMBED THE CHUNKS INTO THE INDEX
 
-from tqdm.auto import tqdm
-from time import sleep
+# Define batch size
+batch_size = 100
 
-batch_size = 100  # how many embeddings we create and insert at once
-
+# Create and insert embeddings in batches
 for i in tqdm(range(0, len(chunks), batch_size)):
-    # find end of batch
+    # Define batch boundary
     i_end = min(len(chunks), i+batch_size)
+    # Get metadata for the batch
     meta_batch = chunks[i:i_end]
-    # get ids
+    # Get IDs for the batch
     ids_batch = [x['id'] for x in meta_batch]
-    # get texts to encode
+    # Get texts to encode
     texts = [x['text'] for x in meta_batch]
-    # create embeddings (try-except added to avoid RateLimitError)
+    # Create embeddings (handle RateLimitError if it occurs)
     try:
         res = openai.Embedding.create(input=texts, engine=embed_model)
     except:
         done = False
         while not done:
-            sleep(5)
+            time.sleep(5)
             try:
                 res = openai.Embedding.create(input=texts, engine=embed_model)
                 done = True
             except:
                 pass
+    # Get embeddings
     embeds = [record['embedding'] for record in res['data']]
-    # cleanup metadata
-    meta_batch = [{
-        'text': x['text'],
-        'chunk': x['chunk'],
-        'url': x['url']
-    } for x in meta_batch]
+    # Clean up metadata
+    meta_batch = [{'text': x['text'], 'chunk': x['chunk'], 'url': x['url']} for x in meta_batch]
     to_upsert = list(zip(ids_batch, embeds, meta_batch))
-    # upsert to Pinecone
+    # Upsert to Pinecone
     index.upsert(vectors=to_upsert)
